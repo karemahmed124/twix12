61d1c48b0d30088f937d86e668362318d0857c15
karemahmed124 <42273386+karemahmed124@users.noreply.github.com>
2019-03-12 20:25:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,47 +1,16 @@
-const Discord = require('discord.js');
-const { Client, Util } = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const { PREFIX, GOOGLE_API_KEY } = require('./config');
-const prefix = '1'
-const YouTube = require('simple-youtube-api');
-const ytdl = require('ytdl-core');
-const youtube = new YouTube(GOOGLE_API_KEY);
-const queue = new Map();
-const moment = require('moment');
-const fs = require("fs");
-const coolDown = new Set();
-const Canvas = require('canvas');
-const db = require('quick.db')
-const ms = require('ms')
-const bot = new Discord.Client();
-
+var prefix = "1";
 client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : EX Clan`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
+    console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`..`,"https://www.twitch.tv/7alabygamer98")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+client.user.setGame(`Crezma ,! [-]`,"http://twitch.tv/Death Shop")
+client.user.setStatus("dnd")
 });
-
-
 
 client.on('warn', console.warn);
 
